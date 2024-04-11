a07a627e00200a1b867a0593c32e3b51be907fec
Jack Hamilton <165817623+jackhamilton-grindr@users.noreply.github.com>
2024-04-11 11:49:06 -0500

    Shift permissive hold, Suraj Kurapati's bilateral combos

--- users/jackhamilton/rules.mk
+++ users/jackhamilton/rules.mk
@@ -8,6 +8,8 @@ TAP_DANCE_ENABLE = yes
 CAPS_WORD_ENABLE = yes
 KEY_OVERRIDE_ENABLE = yes
 
+DEFERRED_EXEC_ENABLE = yes
+
 INTROSPECTION_KEYMAP_C = jack-hamilton_miryoku.c # keymaps
 
 BOOTLOADER = caterina
