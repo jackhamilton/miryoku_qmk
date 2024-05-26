85e6f22f7a39bc91a7ff495fc25b2c4aed81a60a
Jack Hamilton <jackham800@gmail.com>
2024-05-26 14:48:07 -0500

    experimental

--- keyboards/evilkbd_dbg/config.h
+++ keyboards/evilkbd_dbg/config.h
@@ -3,6 +3,15 @@
 
 #pragma once
 
+#define I2C1_SDA_PIN GP28
+#define I2C1_SCL_PIN GP29
+#define OLED_ADDRESS 0x3C
+#define OLED_DISPLAY_128X32
+#define OLED_TIMEOUT 30000  // Timeout in milliseconds
+#define OLED_BRIGHTNESS 128 // Set brightness level
+
+#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET
+#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET_TIMEOUT 200U
 /*
  * Feature disable options
  *  These options are also useful to firmware size reduction.
--- keyboards/evilkbd_dbg/rules.mk
+++ keyboards/evilkbd_dbg/rules.mk
@@ -1,1 +1,4 @@
 # This file intentionally left blank
+OLED_ENABLE = yes
+OLED_DRIVER = SSD1306
+I2C_DRIVER = I2CD1
