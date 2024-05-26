27670344dae07ee6838eb22262a86011539b4b75
Jack Hamilton <jackham800@gmail.com>
2024-05-26 16:02:32 -0500

    modified layout

--- keyboards/evilkbd_dbg/keymaps/default/keymap.c
+++ keyboards/evilkbd_dbg/keymaps/default/keymap.c
@@ -15,10 +15,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * │Ctl│App│GUI│Alt│Spc│Spc│
      * └───┴───┴───┴───┴───┴───┘
      */
-    [0] = LAYOUT_ortho_4x6(
+    [0] = LAYOUT_ortho_3x6(
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,
         KC_BSPC, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,
-        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,
-        KC_LCTL, KC_MENU, KC_LGUI, KC_LALT, KC_SPC,  KC_SPC
+        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B
     )
 };
